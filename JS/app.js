3f839ccb4ad1d2def79be5e66546713b58c6b810
Claudia Vashchenko <78990312+claudiavashchenko@users.noreply.github.com>
2023-05-10 13:57:41 -0500

    displayed api data on browser

--- JS/app.js
+++ JS/app.js
@@ -6,7 +6,10 @@ function getData() {
    fetch('https://api.punkapi.com/v2/beers/random')
    .then(response => response.json())
    .then(data => {
-    console.log(data)
+    const name = data[0].name
+    const description = data[0].description
+    randomBeerTitle.textContent = name
+    randomBeerDescription.textContent = description
    })
 }
 
